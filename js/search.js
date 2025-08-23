428ea704976f68d11a5232deccf83b1443a7771f
ph4xus <maxwell2ste@gmail.com>
2025-08-22 19:35:52 -0700

    literally im crashing out

--- js/search.js
+++ js/search.js
@@ -1,42 +1,47 @@
+const searchContainer = document.querySelector(".search-container");
+
 document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById("search-input");
   const resultsContainer = document.getElementById("search-results");
 
-  let games = [];
+  let gxmes = [];
 
   fetch("../json/list.json")
     .then(res => res.json())
     .then(data => {
-      games = data;
+      gxmes = data;
     });
 
   searchInput.addEventListener("input", () => {
+    searchContainer.style.borderBottomLeftRadius = "0";
+    searchContainer.style.borderBottomRightRadius = "0";
+    document.getElementById("search-results").classList.add("active");
     const query = searchInput.value.toLowerCase();
     resultsContainer.innerHTML = "";
 
     if (query.length === 0) {
-      resultsContainer.style.display = "none";
+      resultsContainer.innerHTML = "Try searching somethin or hit esc";
       return;
     }
 
-    const filtered = games.filter(game =>
-      game.name.toLowerCase().includes(query)
+    const filtered = gxmes.filter(gxme =>
+      gxme.name.toLowerCase().includes(query)
     );
 
     if (filtered.length === 0) {
-      resultsContainer.style.display = "none";
+      resultsContainer.innerHTML = "Nothing found with the search of: " + query;
       return;
     }
 
     resultsContainer.style.display = "block";
 
-    filtered.forEach(game => {
+    filtered.forEach(gxme => {
       const card = document.createElement("div");
       card.className = "search-game-card";
       card.innerHTML = `
-        <a href="${game.linksrc}">
-          <img src="${game.imgsrc}" alt="${game.name}">
-          <h3>${game.name}</h3>
+        <a href="/gxmes/${gxme.foldername}">
+          <img src="${gxme.imgsrc}" alt="${gxme.name}">
+          <h3>${gxme.name}</h3>
         </a>
       `;
       resultsContainer.appendChild(card);
@@ -46,6 +51,9 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("click", (e) => {
     if (!document.querySelector(".search-wrapper").contains(e.target)) {
       resultsContainer.style.display = "none";
+      document.getElementById("search-results").classList.remove("active"); 
     }
+  searchContainer.style.borderBottomLeftRadius = "10px";
+  searchContainer.style.borderBottomRightRadius = "10px";
   });
 });
